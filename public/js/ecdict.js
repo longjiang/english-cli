088b72525fd02872d93f1dc5667700e98a209a07
Jon Long <jon.long@voxelcreative.ca>
2019-09-14 05:29:52 +0200

    Able to use multiple dictionaries.

--- public/js/ecdict.js
+++ public/js/ecdict.js
@@ -1,122 +1,4 @@
 const Dictionary = {
-  dictionaries: {
-    af: {
-      filename: 'eng-afr',
-      parser: 'regular'
-    },
-    ar:  {
-      filename: 'eng-ara',
-      parser: 'regular'
-    },
-    cs:  {
-      filename: 'eng-ces',
-      parser: 'regular'
-    },
-    cy:  {
-      filename: 'eng-cym',
-      parser: 'regular'
-    },
-    da:  {
-      filename: 'eng-dan',
-      parser: 'regular'
-    },
-    de:  {
-      filename: 'eng-deu',
-      parser: 'regular'
-    },
-    en:  {
-      filename: 'eng-fra',
-      parser: 'regular'
-    },
-    eo:  {
-      filename: 'eng-epo',
-      parser: 'regular'
-    },
-    fi:  {
-      filename: 'eng-fin',
-      parser: 'regular'
-    },
-    fr:  {
-      filename: 'eng-fra',
-      parser: 'regular'
-    },
-    ga:  {
-      filename: 'eng-gle',
-      parser: 'regular'
-    },
-    hr:  {
-      filename: 'eng-hrv',
-      parser: 'regular'
-    },
-    hu:  {
-      filename: 'eng-hun',
-      parser: 'regular'
-    },
-    is:  {
-      filename: 'eng-isl',
-      parser: 'regular'
-    },
-    it:  {
-      filename: 'eng-ita',
-      parser: 'regular'
-    },
-    ja:  {
-      filename: 'eng-jpn',
-      parser: 'regular'
-    },
-    kh:  {
-      filename: 'eng-kha',
-      parser: 'regular'
-    },
-    ku:  {
-      filename: 'eng-kur',
-      parser: 'regular'
-    },
-    la:  {
-      filename: 'eng-lat',
-      parser: 'regular'
-    },
-    lt:  {
-      filename: 'eng-lit',
-      parser: 'regular'
-    },
-    nl:  {
-      filename: 'eng-nld',
-      parser: 'regular'
-    },
-    pl:  {
-      filename: 'eng-pol',
-      parser: 'regular'
-    },
-    pt:  {
-      filename: 'eng-por',
-      parser: 'regular'
-    },
-    sk:  {
-      filename: 'eng-slk',
-      parser: 'regular'
-    },
-    es:  {
-      filename: 'eng-spa',
-      parser: 'regular'
-    },
-    sr:  {
-      filename: 'eng-srp',
-      parser: 'regular'
-    },
-    sv:  {
-      filename: 'eng-swe',
-      parser: 'regular'
-    },
-    sw:  {
-      filename: 'eng-swh',
-      parser: 'regular'
-    },
-    tr:  {
-      filename: 'eng-tur',
-      parser: 'regular'
-    },
-  },
   lang: undefined,
   file: undefined,
   words: [],
@@ -124,83 +6,43 @@ const Dictionary = {
   cache: {},
   tables: [],
   loadWords() {
+    console.log('Loading words...')
     return new Promise(resolve => {
-      console.log('FreeDict: Loading words')
-      let xhttp = new XMLHttpRequest()
-      let that = this
-      xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-          that.words = that.parseDictionary(this.responseText)
+      Papa.parse(this.file, {
+        download: true,
+        header: true,
+        complete: results => {
+          let words = []
+          for (let index in results.data) {
+            let row = results.data[index]
+            words.push(this.augment(row, index))
+          }
+          this.words = words
+          console.log('Words loaded.')
           resolve()
         }
-      }
-      xhttp.open('GET', this.file, true)
-      xhttp.send()
+      })
     })
   },
-  parseLines(lines) {
-    let words = []
-    for (let index in lines) {
-      index = parseInt(index)
-      let english = lines[index]
-      let matches = english.match(/(.*) \/(.*)\//)
-      if (matches) {
-        let matches2 = english.match(/<(.*)>/)
-        // Parse definitions (sometimes it's one line, other times it's multiple lines with numeric headings)
-        let i = 1
-        let isDef = !lines[index + i].match(/(.*) \/(.*)\//)
-        let definitions = []
-        while (isDef) {
-          let def = lines[index + i].replace(/^\d+\./, '').trim()
-          if (def !== '') {
-            definitions.push(def)
-          }
-          i = i + 1
-          if (lines[index + i]) {
-            isDef = !lines[index + i].match(/(.*) \/(.*)\//)
-          } else {
-            isDef = false
-          }
-        }
-        let pronunciation = matches ? matches[2] : undefined
-        let word = {
-          bare: matches ? matches[1] : undefined,
-          head: matches
-            ? matches[1].replace(/\(.*\)\/ /, '').toLowerCase()
-            : undefined,
-          pronunciation: ['fi', 'lt', 'cy', 'ku'].includes(this.lang)? undefined : pronunciation,
-          definitions: definitions,
-          pos: matches2 && matches2.length > 1 ? matches2[1] : undefined
-        }
-        word.accented = word.bare
-        words.push(word)
-      }
+  augment(row, id) {
+    let word = {
+      id: id,
+      bare: row.word,
+      accented: row.word,
+      head: row.word,
+      pronunciation: row.phonetic,
+      definitions: [row.translation],
+      pos: row.pos,
+      extra: row
     }
-    return words
-  },
-  parseDictionary(text) {
-    text = text.replace(/^[^\n]*\n/m, '') // remove title line
-    console.log('Parsing FreeDict Dictionary from ' + this.file)
-    let lines = text.split('\n')
-    let words = []
-    words = this.parseLines(lines)
-    words = words.sort((a, b) => {
-      if (a.head && b.head) {
-        return a.head.length - b.head.length
-      }
-    })
-    words = words.map((word, index) => {
-      word.id = index
-      return word
-    })
-    return words
+    return word
   },
   load(lang) {
     console.log('Loading ECDICT...')
     this.lang = lang
     let server = 'http://hsk-server.local:8888/'
     // let server = 'https://server.chinesezerotohero.com/'
-    this.file = `${server}data/freedict/${this.dictionaries[this.lang].filename}.dict.txt`
+    this.file = `${server}data/ecdict/ecdict.csv.txt`
     return new Promise(async resolve => {
       let promises = [this.loadWords()]
       await Promise.all(promises)
